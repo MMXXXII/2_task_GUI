c0f30bdf52ea2d3cdc529cafee2e82b89d4258b2
MMXXII <perfilev621@gmail.com>
2025-03-13 12:04:13 +0800

    код сделан более легче и меньше

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -8,70 +8,51 @@ namespace WinFormsApp1
         public Form1()
         {
             InitializeComponent();
-
             // Восстановление данных при открытии
             textBox1.Text = Properties.Settings.Default.SideA;
             textBox2.Text = Properties.Settings.Default.SideB;
             textBox3.Text = Properties.Settings.Default.SideC;
-
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (TryGetSides(out int a, out int b, out int c))
-            {
-                if (Triangle.Exists(a, b, c))
-                {
-                    MessageBox.Show($"Треугольник со сторонами {a}, {b}, {c} существует", "Проверка существования");
-                }
-                else
-                {
-                    MessageBox.Show($"Треугольник со сторонами {a}, {b}, {c} не существует", "Ошибка");
-                }
-            }
+            CheckTriangle((a, b, c) => Triangle.Exists(a, b, c), "существует");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            if (TryGetSides(out int a, out int b, out int c) && Triangle.Exists(a, b, c))
+            {
+                MessageBox.Show(Triangle.IsRightTriangle(a, b, c) ? "Треугольник является прямоугольным" : "Треугольник не является прямоугольным", "Проверка на прямоугольность");
+            }
+            else
+            {
+                MessageBox.Show("Треугольник не существует, проверка на прямоугольность невозможна", "Ошибка");
+            }
+        }
+
+        private void CheckTriangle(Func<int, int, int, bool> triangleCheck, string checkType)
+        {
             if (TryGetSides(out int a, out int b, out int c))
             {
-                if (Triangle.Exists(a, b, c))
-                {
-                    if (Triangle.IsRightTriangle(a, b, c))
-                    {
-                        MessageBox.Show("Треугольник является прямоугольным", "Проверка на прямоугольность");
-                    }
-                    else
-                    {
-                        MessageBox.Show("Треугольник не является прямоугольным", "Проверка на прямоугольность");
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("Треугольник не существует, проверка на прямоугольность невозможна", "Ошибка");
-                }
+                string message = triangleCheck(a, b, c)
+                    ? $"Треугольник со сторонами {a}, {b}, {c} {checkType}"
+                    : $"Треугольник со сторонами {a}, {b}, {c} не {checkType}";
+                MessageBox.Show(message, $"Проверка {checkType}");
             }
         }
 
         private bool TryGetSides(out int a, out int b, out int c)
         {
-            // Присвоение значений по умолчанию для out-параметров
             a = b = c = 0;
-
-            bool isValid = int.TryParse(textBox1.Text, out a)
-                        && int.TryParse(textBox2.Text, out b)
-                        && int.TryParse(textBox3.Text, out c);
-
+            bool isValid = int.TryParse(textBox1.Text, out a) && int.TryParse(textBox2.Text, out b) && int.TryParse(textBox3.Text, out c);
             if (!isValid || a <= 0 || b <= 0 || c <= 0)
             {
                 MessageBox.Show("Введите корректные положительные целые числа", "Ошибка ввода");
                 return false;
             }
-
             return true;
         }
-
-
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
@@ -81,19 +62,12 @@ namespace WinFormsApp1
             Properties.Settings.Default.SideC = textBox3.Text;
             Properties.Settings.Default.Save();
         }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 
     public class Triangle
     {
-        public static bool Exists(int a, int b, int c)
-        {
-            return a > 0 && b > 0 && c > 0 && (a + b > c) && (a + c > b) && (b + c > a);
-        }
+        public static bool Exists(int a, int b, int c) =>
+            a > 0 && b > 0 && c > 0 && (a + b > c) && (a + c > b) && (b + c > a);
 
         public static bool IsRightTriangle(int a, int b, int c)
         {
@@ -103,4 +77,3 @@ namespace WinFormsApp1
         }
     }
 }
-
